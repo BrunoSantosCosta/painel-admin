3841079a8d7df2fb6444d07bcb9d6e680bfba993
Bruno Santos <brunosanttos667@gmail.com>
2023-01-26 19:27:22 -0800

    Initial Commit

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0x0ff26fff);
+const secondaryColor = Color(0XFF2A2D3E);
+const bgColor = Color(0XFF212332);
+
+const defaultPadding = 16;
---
+++ lib/screen/main/main_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+                color: Colors.blue,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

1e2eb3bbf033404dd25cf62e923b76af879fa450
Bruno Santos <brunosanttos667@gmail.com>
2023-01-30 18:57:20 -0800

    Recent Files build

---
+++ lib/models/RecentFiles.dart
@@ -0,0 +1,43 @@
+class RecentFile {
+  final String icon, title, date, size;
+
+  RecentFile({
+    required this.icon,
+    required this.title,
+    required this.date,
+    required this.size,
+  });
+}
+
+List demoRecentFiles = [
+  RecentFile(
+    icon: 'assets/icons/xd_file.svg',
+    title: 'XD File',
+    date: '01-03-2021',
+    size: '3.5mb',
+  ),
+  RecentFile(
+    icon: 'assets/icons/xd_file.svg',
+    title: 'XD File',
+    date: '01-03-2021',
+    size: '3.5mb',
+  ),
+  RecentFile(
+    icon: 'assets/icons/xd_file.svg',
+    title: 'XD File',
+    date: '01-03-2021',
+    size: '3.5mb',
+  ),
+  RecentFile(
+    icon: 'assets/icons/xd_file.svg',
+    title: 'XD File',
+    date: '01-03-2021',
+    size: '3.5mb',
+  ),
+  RecentFile(
+    icon: 'assets/icons/xd_file.svg',
+    title: 'XD File',
+    date: '01-03-2021',
+    size: '3.5mb',
+  ),
+];
